fed28973ee957e604f86f08238166d4fe8318292
HaykLach <lachikyanhayk02@gmail.com>
2022-12-20 16:31:31 +0400

    design fixes

--- resources/views/web/packages.blade.php
+++ resources/views/web/packages.blade.php
@@ -4,7 +4,7 @@
         <div class="service-package-content">
             <h1>Packages</h1>
             @forelse($packages as $key => $serviceName)
-                <h2>{{ $key }}</h2>
+                <h2 class="packages-subTitle">{{ $key }}</h2>
                 <div class="services-packages packages">
                     @foreach($serviceName as $package)
                         @include('web.package_data')
